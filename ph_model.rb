4b664893ac9ca4cef6286e20a576b7ba60700db7
vit <none@none>
2011-07-26 21:21:43 +0000

    init

---
+++ ph_model.rb
@@ -0,0 +1,21 @@
+module Physcon
+	class Model
+		def initialize args={}
+		end
+	end
+	class App
+		class << self
+			attr_reader :config, :model
+		end
+		def self.init file
+			config0 = YAML::load( open(file, "r:UTF-8") )
+		#	puts config0
+			env = Rails.env
+			@config = config0 && config0[env] ? config0[env] : {}
+			@model = Model.new @config
+		#	puts @config
+		#	puts Rails.env
+		end
+	end
+end
+
