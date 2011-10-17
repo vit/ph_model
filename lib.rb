6c873286d7118628f41b9741102927be07e4e353
vit <none@none>
2011-10-17 19:44:17 +0000

    m

--- lib.rb
+++ lib.rb
@@ -4,22 +4,37 @@
 
 module Physcon
 	class Lib
+		LIB_DOC_CLASS = 'LIB:DOC'
 		attr_reader :mongo, :lib
 		def initialize model, config={}
 			@model = model
 			@docs = model.mongo['docs']
 		end
-		def new_doc id
+		def new_doc parent, dir, info
 			_id = Physcon::SEQ[]
+			ts = Physcon::TS[]
+			@docs.insert({
+				_id: _id,
+				_meta: {class: LIB_DOC_CLASS, parent: parent, dir: dir, ctime: ts, mtime: ts},
+				info: info
+			})
 			{
 				_id: _id
 			}
 		end
 		def get_doc_info id
-			{
-				title: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-				abstract: 'wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww'
-			}
+			res = @docs.find_one({'_meta.class' => LIB_DOC_CLASS, '_id' => id})
+			res ? res['info'] : nil
+		end
+		def get_doc_children id
+			@docs.find(
+				{'_meta.class' => LIB_DOC_CLASS, '_meta.parent' => id}
+			).map do |d|
+				{
+					_id: d['_id'],
+					info: d['info']
+				}
+			end
 		end
 	end
 end
