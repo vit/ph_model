83ec47e89807b391b6b627092d639d68aa2e68f0
vit <none@none>
2011-10-30 22:09:58 +0000

    m

--- coms.rb
+++ coms.rb
@@ -27,8 +27,28 @@ module Physcon
 		end
 		def get_conf_paper_info id, papnum
 			id = id.to_i
+			papnum = papnum.to_i
 			Physcon::App.model.pg.query_one "SELECT p.*, concatpaperauthors(%s, p.papnum) as authors FROM paper as p where p.context=%s and p.papnum=%s and p.finaldecision>1", id, id, papnum
 		end
+		def get_conf_paper_file id, papnum
+			id = id.to_i
+			papnum = papnum.to_i
+			rez = nil
+	#		row = Physcon::App.model.pg.query_one "SELECT * FROM paper WHERE context=%s AND papnum=%s", id, papnum
+			row = @model.pg.query_one "SELECT * FROM paper WHERE context=%s AND papnum=%s", id, papnum
+			if row && row['filetype'] && row['filetype'].length > 0
+				file_path = Physcon::App.config['coms']['papers_path']+"c#{id}p#{papnum}"
+	#			#file = File.open file_path
+				rez = {
+					file_path: file_path,
+					#file: file,
+					original_filename: row['filename'],
+					content_type: row['filetype']
+				}# if file
+			end
+			rez
+		end
+
 	end
 end
 
--- lib.rb
+++ lib.rb
@@ -6,10 +6,14 @@ module Physcon
 	class Lib
 		#LIB_DOC_CLASS = 'LIB:DOC:PAPER'
 		LIB_DOC_CLASS = 'LIB:DOC'
+		LIB_DOC_FILE_CLASS = 'LIB:DOC:FILE'
 		attr_reader :mongo, :lib
 		def initialize model, config={}
+			@db = model.mongo
 			@model = model
-			@docs = model.mongo['docs']
+			@docs = @db['docs']
+			@grid = Mongo::Grid.new @db, 'docs'
+			@files = @db['docs.files']
 		end
 		def new_doc parent, dir, info
 			_id = Physcon::SEQ[]
@@ -18,11 +22,11 @@ module Physcon
 				_id: _id,
 				_meta: {class: LIB_DOC_CLASS, parent: parent, dir: dir, ctime: ts, mtime: ts},
 				info: info
-				#info: {'title' => 'qqq %', 'abstract' => 'www %'}
 			})
-			{
-				'_id' => _id
-			}
+		#	{
+		#		'_id' => _id
+		#	}
+			_id
 		end
 		def get_doc_info id
 			res = @docs.find_one({'_meta.class' => LIB_DOC_CLASS, '_id' => id})
@@ -31,6 +35,45 @@ module Physcon
 		def set_doc_info id, info
 			@docs.update({'_meta.class' => LIB_DOC_CLASS, '_id' => id}, {'$set' => {'info' => info, '_meta.mtime' => Physcon::TS[]}})
 			{_id: id}
+		end
+		def remove_doc id
+			if get_doc_children(id).empty?
+				remove_doc_file id
+				@docs.remove({
+					_id: id,
+					'_meta.class' => LIB_DOC_CLASS
+				})
+			end
+		end
+		def remove_docs list
+			list.each do |doc|
+				remove_doc doc['_id']
+			end
+		end
+		def put_doc_file doc_id, input, args={}
+			remove_doc_file doc_id
+			ts = Physcon::TS[]
+			_id = Physcon::SEQ[]
+			args.merge!({_id: _id, _meta: {
+				class: LIB_DOC_FILE_CLASS,
+				parent: doc_id,
+				ctime: ts,
+				mtime: ts
+			}})
+			@grid.put input, args
+			_id
+		end
+		def get_doc_file id
+			@grid.get id if id
+		end
+		def remove_doc_file doc_id
+			id = find_doc_file(doc_id)
+			@grid.delete id if id
+		end
+		def find_doc_file doc_id
+			res = @files.find_one( {'_meta.class' => LIB_DOC_FILE_CLASS, '_meta.parent' => doc_id} )
+			#res = @files.find_one( {} )
+			res ? res['_id'] : nil
 		end
 		def get_doc_children id
 			@docs.find(
@@ -60,8 +103,18 @@ module Physcon
 			rez
 		end
 		def import_doc_from_coms id, context, papnum
+			doc_id = nil
 			d = @model.coms.get_conf_paper_info context, papnum
-			new_doc(id, false, {title: d['title'], abstract: d['abstract']})
+			if d
+				doc_id = new_doc(id, false, {title: d['title'], abstract: d['abstract']})
+				file_info = @model.coms.get_conf_paper_file context, papnum
+				if file_info
+					File.open file_info[:file_path] do |f|
+						put_doc_file doc_id, f, file_info
+					end
+				end
+			end
+			doc_id
 		end
 		def import_docs_from_coms id, list
 			list.each do |doc|
@@ -71,6 +124,7 @@ module Physcon
 			end
 		end
 		def remove_doc id
+			remove_doc_file id
 			@docs.remove({
 				_id: id,
 				'_meta.class' => LIB_DOC_CLASS
